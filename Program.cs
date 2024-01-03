619ba15fa19c213a1cc9777a2817e0e1723e231a
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2024-01-03 12:19:47 -0700

    set properties such as id size and sequence position

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MultipleInheritance
+{
+    public class UiControl
+    {
+        public string Id { get; set; }
+        public Size Size { get; set; }
+        public SequencePosition TopLeft { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+        public void Focus()
+        {
+            Console.WriteLine("RECEIVED FOCUS");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UiControl.cs
@@ -0,0 +1,18 @@
+﻿namespace MultipleInheritance
+{
+    public class UiControl
+    {
+        public string Id { get; set; }
+        public Size Size { get; set; }
+        public SequencePosition TopLeft { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+        public void Focus()
+        {
+            Console.WriteLine("RECEIVED FOCUS");
+        }
+    }
+}
